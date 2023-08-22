b249e286a68ab7ca839dec7fd86c5ef8baffb381
nishcreed <nishaanth27rams@gmail.com>
2023-08-22 20:42:40 +0530

    session

--- server/app.js
+++ server/app.js
@@ -77,7 +77,7 @@ const sessionConfig = {
 
 if(process.env.NODE_ENV == "production"){
   app.set("trust proxy", 1);
-  // sessionConfig.proxy=true;
+  sessionConfig.proxy=true;
   sessionConfig.cookie.sameSite='none';
   // sessionConfig.secure = true,
   sessionConfig.cookie.domain='.onrender.com';
